a1c356ed7a666bc783c1b52aa13b2e93a2e04fe2
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-06-04 17:56:01 +0530

    Update gas station.cpp

--- Greedy/gas station.cpp
+++ Greedy/gas station.cpp
@@ -1,1 +1,26 @@
 
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
+        if(accumulate(gas.begin(),gas.end(),0) < accumulate(cost.begin(),cost.end(),0)) {
+            return -1;
+        }
+        int n = gas.size();
+        int total = 0;
+
+
+        int current = 0;
+        for(int i =0;i<n ;i++) {
+            total += (gas[i] - cost[i]);
+            if(total < 0) {
+                total = 0;
+                current = i+1;
+            }
+           
+        }
+        return current;
+
+        
+    }
+};
