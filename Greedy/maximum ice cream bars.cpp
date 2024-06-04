59eb991238a0cee04c05426853a38b63c71b8396
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-06-04 17:46:53 +0530

    Update maximum ice cream bars.cpp

--- Greedy/maximum ice cream bars.cpp
+++ Greedy/maximum ice cream bars.cpp
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+
+        int ans = 0;
+
+        for(auto it : costs) {
+            if(it <= coins) {
+                ans++;
+                coins -= it;
+            }
+            else {
+                break;
+
+            }
+        }
+        return ans;
+        
+    }
+};
