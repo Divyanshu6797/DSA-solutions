87df0250ac413bc06cd30689b71d852459a389bf
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-06-11 22:13:22 +0530

    Update matrix chain multiplication.cpp

--- DP/Partioning DP/matrix chain multiplication.cpp
+++ DP/Partioning DP/matrix chain multiplication.cpp
@@ -1,1 +1,52 @@
+//memoization
+class Solution{
+public:
 
+    int solve(int i,int j,int arr[], vector<vector<int>> &dp) {
+        if(i==j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int mini = 1e9;
+        for(int k = i; k<j ; k++) {
+            int res = arr[i-1]*arr[k]*arr[j] + solve(i,k,arr,dp) + solve(k+1,j,arr,dp);
+            if(res < mini) {
+                mini = res;
+            }
+            
+            
+        }
+        return dp[i][j]=mini;
+        
+        }
+    int matrixMultiplication(int N, int arr[])
+    {
+        vector<vector<int>> dp(N,vector<int>(N,-1));
+        return solve(1,N-1,arr,dp);
+    }
+};
+
+
+//Tabulation
+
+class Solution{
+public:
+    int matrixMultiplication(int N, int arr[])
+    {
+        vector<vector<int>> dp(N,vector<int>(N,0));
+        
+        for(int i = N-1 ; i>=1 ;i--) {
+            for(int j = i+1; j<N ; j++) {
+                int mini = 1e9;
+                for(int  k = i; k<j;k++) {
+                    int res = arr[i-1]*arr[k]*arr[j] + dp[k+1][j] + dp[i][k];
+                    if(res < mini) {
+                        mini = res;
+                    }
+                    
+                    
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][N-1];
+    }
+};
