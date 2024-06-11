8e3940217578ec15c241af2187a44179845bc2ba
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-06-11 11:23:04 +0530

    Update Swap two numbers.cpp

--- Bit Manipulation/Basics/Swap two numbers.cpp
+++ Bit Manipulation/Basics/Swap two numbers.cpp
@@ -1,1 +1,28 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void swapArithmetic(int& a, int& b) {
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After: a = " << a << ", b = " << b << endl;
+}
+
+
+void swapBitManipulation(int& a, int& b) {
+  
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout << "After: a = " << a << ", b = " << b << endl;
+}
+
+int main() {
+    int a = 10, b = 20;
+    swapArithmetic(a, b);
+    a = 10, b = 20;
+    swapBitManipulation(a, b);
+    return 0;
+}
