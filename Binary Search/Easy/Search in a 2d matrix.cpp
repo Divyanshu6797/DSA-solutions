2480da1a24cba08d7b3a36b5884eb370f308ac30
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-06-19 23:24:49 +0530

    Update Search in a 2d matrix.cpp

--- Binary Search/Easy/Search in a 2d matrix.cpp
+++ Binary Search/Easy/Search in a 2d matrix.cpp
@@ -23,3 +23,37 @@ public:
         
     }
 };
+
+// TC ->  O(log m*n )
+// solve like the 2d array is flattened
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int low = 0;
+        int high = n*m - 1;
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] < target) {
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+
+        }
+        return false;
+        
+    }
+};
